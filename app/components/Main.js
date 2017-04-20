81cdde32b72a19420af04fed7dcb3c4955a1d146
Kajetan Mazur <kajetan@webownia.pl>
2017-04-20 17:47:32 +0200

    -practice react - github-notetaker

---
+++ app/components/Main.js
@@ -0,0 +1,18 @@
+var React = require('react');
+
+var Main = React.createClass({
+  render: function(){
+    return (
+      <div className="main-container">
+        <nav className="navbar navbar-default" role="navigation">
+          <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
+            MENU
+          </div>
+        </nav>
+        <div>{this.props.children}</div>
+      </div>
+    )
+  }
+});
+
+module.exports = Main;
